409156bded003ee3eee4ab1626fdd6a129216ef4
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-08-06 16:06:24 -0600

    adding logs to reusable pool

--- util/reusable_pool.py
+++ util/reusable_pool.py
@@ -1,3 +1,5 @@
+import logging
+import time
 from multiprocessing.pool import ThreadPool
 
 
@@ -7,13 +9,19 @@ class ReusablePool:
     """
 
     def __init__(self, size, constructor, clean_up):
+        start = time.time()
+        self.logger = logging.getLogger(__name__)
+        self.logger.info('Initiating instances...')
         with ThreadPool(size) as tp:
             self._reusables = tp.map(constructor, range(size))
             self._clean_up = clean_up
+        self.logger.info(f'Instances initiated. Took {time.time() - start} (s).')
 
     def acquire(self):
+        self.logger.info('Acquiring an instance.')
         return self._reusables.pop()
 
     def release(self, reusable):
+        self.logger.info('Releasing an instance.')
         self._clean_up(reusable)
         self._reusables.append(reusable)
