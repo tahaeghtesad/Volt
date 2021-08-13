9910384c141c93cd6c5482688d12c6b5f18b4df2
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-08-12 22:36:12 -0600

    reusable is not reusable at all

--- util/reusable_pool.py
+++ util/reusable_pool.py
@@ -18,13 +18,17 @@ class ReusablePool:
         # self.logger.info(f'Instances initiated. Took {time.time() - start} (s).')
         self.constructor = constructor
         self.clean_up = clean_up
+        self.created = 0
+        self.deleted = 0
 
     def acquire(self):
-        self.logger.info('Acquiring an instance.')
-        return self.constructor()
+        self.logger.info(f'Acquiring an instance. Running: {self.created - self.deleted}')
+        self.created += 1
+        return self.constructor(self.created)
 
     def release(self, reusable):
-        self.logger.info('Releasing an instance.')
+        self.logger.info(f'Releasing an instance. Running: {self.created - self.deleted}')
+        self.deleted -= 1
         # self._clean_up(reusable)
         # self._reusables.append(reusable)
         reusable.close()
